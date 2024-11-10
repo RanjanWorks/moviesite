5cee568e144f5e7e4c7d0dd8a933a420ab54a262
Ranjan kashyap <kashyapranjan9977@gmail.com>
2024-11-10 19:52:48 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
     images: {
         domains: ['via.placeholder.com', 'image.tmdb.org'], // Add any other external image domains here
       },
